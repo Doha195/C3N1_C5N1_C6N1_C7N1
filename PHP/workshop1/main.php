c46c497b3e538969e2bf873eb3526f0b7b9491cd
doha <dohaferiadi@gmail.com>
2021-03-03 09:16:09 +0100

    workshop1

--- PHP/workshop1/main.php
+++ PHP/workshop1/main.php
@@ -3,8 +3,9 @@ $char ="hello world<br>";
 echo $char;
 $a = 44;
 echo $a;
-$hello =  "j'ai 60 ans";
+$hello =  "j'ai 60 ans <br>";
 echo $hello;
-$char1="hello j'ai $a <br>";
+$char1= "hello j'ai $a ans <br>";
 echo $char1;
+
 ?>
